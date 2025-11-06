05caaa1cb9ceb6f4ee2f98b5f018162867d37498
aliabueldahab <aliabueldahab2005@gmail.com>
2025-11-06 18:05:22 +0200

    Add startup logs

--- src/app.js
+++ src/app.js
@@ -23,4 +23,4 @@ app.use('/tasks', takskRoute);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`✅ Server running on port ${PORT}`));
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -5,7 +5,8 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(401).json({
+      return
+       res.status(401).json({
         message: "email and password are required",
       });
     }
